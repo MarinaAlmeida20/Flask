3e66efe9d13a639ca7fa8a14bdac283772d9ec42
Marina <marinabrito.ma@gmail.com>
2020-07-22 22:25:06 +0100

    pages

--- base/tutorial1.py
+++ base/tutorial1.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def home():
     return "Hello! this is the main page <h1>HELLO</h1>"
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run()
